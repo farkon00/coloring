6aa7ded3dca4e268000fe1c2ee101a75ca6754b3
Robin Chapple <r.chapple.business@gmail.com>
2022-05-09 10:01:57 +0100

    Added some set colors

--- coloring/coloring.py
+++ coloring/coloring.py
@@ -5,9 +5,28 @@ class Coloring:
     A class to color and style text
     """
 
+    set_colors = {
+        "red": [255, 0, 0],
+        "orange": [255, 127, 0],
+        "yellow": [255, 255, 0],
+        "green": [0, 255, 0],
+        "lightblue": [0, 255, 255],
+        "blue": [0, 0, 255],
+        "purple": [255, 0, 255],
+    }
+
     def __init__(self):
         pass
 
+    """
+    Print some text with a specific style
+    
+    Arguments:
+        text: The text to be printed and styled
+        style: An (optional) style string to dictate how the text will be styled
+    Returns:
+        Nothing
+    """
     def print(self, text, style=None):
         if style == None:
             print(text)
@@ -20,4 +39,11 @@ class Coloring:
             if rgbMatch:
                 [placeholder, r, g, b] = map(lambda x: x.strip(), re.split('[:,]', rgbMatch.group()))
                 output += '\033[38;2;' + r + ';' + g + ';' + b + 'm'
+            else:
+                color_set = False
+                for key in self.set_colors.keys():
+                    if key in style:
+                        color_set = True
+                        color = self.set_colors[key]
+                        output += '\033[38;2;' + str(color[0]) + ';' + str(color[1]) + ';' + str(color[2]) + 'm'
             print(output + text + escape)
--- example.py
+++ example.py
@@ -3,3 +3,4 @@ from coloring import Coloring
 color = Coloring()
 
 color.print("Hello, green world!", "RGB: 0, 255, 0")
+color.print("Using a set color!", "red")
