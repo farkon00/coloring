69bc1344fab593a9c6a7f871b086fad2b9611735
farkon00 <sammer2016sammer@gmail.com>
2022-05-09 13:48:03 +0300

    Type annotations added, code quality changes

--- coloring/coloring.py
+++ coloring/coloring.py
@@ -8,44 +8,41 @@ class Coloring:
     A class to color and style text
     """
 
-    formatting = {
+    formatting: dict[str, str] = {
         "bold": "\033[1m",
         "italic": "\033[3m",
         "underline": "\033[4m",
         "strikethrough": "\033[9m",
     }
 
-    set_colors = {
-        "white" : [255, 255, 255],
-        "red": [255, 0, 0],
-        "orange": [255, 127, 0],
-        "yellow": [255, 255, 0],
-        "green": [0, 255, 0],
-        "lightblue": [0, 255, 255],
-        "blue": [0, 0, 255],
-        "purple": [255, 0, 255],
+    set_colors: dict[str, tuple[int, int, int]] = {
+        "white" : (255, 255, 255),
+        "red": (255, 0, 0),
+        "orange": (255, 127, 0),
+        "yellow": (255, 255, 0),
+        "green": (0, 255, 0),
+        "lightblue": (0, 255, 255),
+        "blue": (0, 0, 255),
+        "purple": (255, 0, 255),
     }
 
-    def __init__(self):
-        pass
-    """
-    Simply clears the console
-    """
-
-    def clear(self):
+    def clear(self) -> None:
+        """
+        Simply clears the console
+        """
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    """
-    Set the cursor position within the console
 
-    Arguments:
-        x: The x position of the cursor
-        y: The y position of the cursor
-    Returns:
-        success: A boolean indicating whether the cursor position was set successfully
-    """
+    def setCursor(self, x: int, y: int) -> bool:
+        """
+        Set the cursor position within the console
 
-    def setCursor(self, x, y):
+        Arguments:
+            x: The x position of the cursor
+            y: The y position of the cursor
+        Returns:
+            success: A boolean indicating whether the cursor position was set successfully
+        """
         if os.name != 'nt':
             print("\033[%d;%dH" % (y, x))
             return True
@@ -53,26 +50,29 @@ class Coloring:
             # TODO: Move the cursor on Windows
             return False
 
-    """
-    Print some text with a specific style
-    
-    Arguments:
-        text: The text to be printed and styled
-        style: An (optional) style string to dictate how the text will be styled
-    Returns:
-        Nothing
-    """
-    def print(self, text, style=None):
+    def print(self, text: str, style: str | Style | None = None) -> None:
+        """
+        Print some text with a specific style
+        
+        Arguments:
+            text: The text to be printed and styled
+            style: An (optional) style string to dictate how the text will be styled or Style object
+        Returns:
+            Nothing
+        """
         escape = '\033[0m'
-        if style == None:
+        if not style:
             print(text)
             return
+
         elif isinstance(style, Style):
             output = style.generate_string(self.set_colors, self.formatting)
+
         else:
             output = ''
             escape = '\033[0m'
-            for key in self.formatting.keys():
+
+            for key in self.formatting:
                 if re.search(r"\b" + re.escape(key) + r"\b", style):
                     output += self.formatting[key]
 
@@ -80,12 +80,12 @@ class Coloring:
             rgb = re.compile("RGB:\s?\d{1,3},\s?\d{1,3},\s?\d{1,3}")
             rgbMatch = rgb.match(style)
             if rgbMatch:
-                [placeholder, r, g, b] = map(lambda x: x.strip(), re.split('[:,]', rgbMatch.group()))
-                output += '\033[38;2;' + r + ';' + g + ';' + b + 'm'
+                _, r, g, b = map(lambda x: x.strip(), re.split('[:,]', rgbMatch.group()))
+                output += f'\033[38;2;{r};{g};{b}m'
             else:
-                color_set = False
-                for key in self.set_colors.keys():
+                for key in self.set_colors:
                     if re.search(r"\b" + re.escape(key) + r"\b", style):
                         color = self.set_colors[key]
-                        output += '\033[38;2;' + str(color[0]) + ';' + str(color[1]) + ';' + str(color[2]) + 'm'
+                        output += f'\033[38;2;{color[0]};{color[1]};{color[2]}m'
+
         print(output + text + escape)
--- coloring/style.py
+++ coloring/style.py
@@ -1,23 +1,24 @@
 import re
 
 class Style:
-    def __init__(self, color='white', formatting=None):
+    def __init__(self, color: tuple[int, int, int] | str = 'white',
+     formatting: None | list[str] = None) -> None:
         self.color = color
         self.formatting = formatting if formatting is not None else []
 
-    def _set_formatting(self, formatting, value):
+    def _set_formatting(self, formatting: str, value: bool) -> None:
         if value:
             if formatting not in self.formatting:
                 self.formatting.append(formatting)
         else:
             self.formatting.remove(formatting)
 
-    def set_bold(self, value): self._set_formatting('bold', value)
-    def set_italic(self, value): self._set_formatting('italic', value)
-    def set_underline(self, value): self._set_formatting('underline', value)
-    def set_strikethrough(self, value): self._set_formatting('strikethrough', value)
+    def set_bold(self, value: bool) -> None: self._set_formatting('bold', value)
+    def set_italic(self, value: bool) -> None: self._set_formatting('italic', value)
+    def set_underline(self, value: bool) -> None: self._set_formatting('underline', value)
+    def set_strikethrough(self, value: bool) -> None: self._set_formatting('strikethrough', value)
 
-    def generate_string(self, set_colors, formatting):
+    def generate_string(self, set_colors: dict[str, tuple[int, int, int]], formatting: dict[str, str]) -> str:
         rgb = re.compile("RGB:\s?\d{1,3},\s?\d{1,3},\s?\d{1,3}")
         output = ''
         for form in self.formatting:
@@ -25,7 +26,11 @@ class Style:
                 raise ValueError("Unknown formatting: " + str(form))
             output += formatting[form]
 
-        if self.color in set_colors:
+        rgbmatch = None
+        if isinstance(self.color, str):
+            rgbmatch = rgb.match(self.color)
+
+        if isinstance(self.color, str) and self.color in set_colors:
             color = set_colors[self.color]
             output += '\033[38;2;' + str(color[0]) + ';' + str(color[1]) + ';' + str(color[2]) + 'm'
         elif isinstance(self.color, tuple | list):
@@ -33,8 +38,8 @@ class Style:
                 raise ValueError("Color must be a tuple of length 3")
 
             output += '\033[38;2;' + str(self.color[0]) + ';' + str(self.color[1]) + ';' + str(self.color[2]) + 'm'
-        elif rgb.match(self.color):
-            _, r, g, b = map(lambda x: x.strip(), re.split('[:,]', rgb.match(self.color).group()))
+        elif rgbmatch:
+            _, r, g, b = map(lambda x: x.strip(), re.split('[:,]', rgbmatch.group()))
             output += '\033[38;2;' + r + ';' + g + ';' + b + 'm'
 
         return output
